3ef1aa22313844d25d7c4c8862229024efdf0aa6
dipenarathod <dipenArathod@gmail.com>
2024-07-30 01:27:48 +0530

    Update Test1.py

--- Test1.py
+++ Test1.py
@@ -85,3 +85,49 @@ if(j<10):
 
 if(k>=10):
     print("k>=10")
+
+n = l + m + i + j
+
+if(i>10):
+    print("i wins")
+
+if(j<10):
+    print("j<10")
+
+if(k>=10):
+    print("k>=10")
+
+
+n = l + m + i + j
+
+if(i>10):
+    print("i wins")
+
+if(j<10):
+    print("j<10")
+
+if(k>=10):
+    print("k>=10")
+
+n = l + m + i + j
+
+if(i>10):
+    print("i wins")
+
+if(j<10):
+    print("j<10")
+
+if(k>=10):
+    print("k>=10")
+
+
+n = l + m + i + j
+
+if(i>10):
+    print("i wins")
+
+if(j<10):
+    print("j<10")
+
+if(k>=10):
+    print("k>=10")
